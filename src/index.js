bd138e544009169cdc3305f22dc79f9bb9a71d9e
sangitasarkar74 <sangitasarkar@gmail.com>
2023-02-23 14:25:51 +0530

    changes updated

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import repairList from './reducer';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import repairList from "./reducer";
 
 // Store is defined with the reducer
 
-const store = createStore(repairList,
+const store = createStore(
+  repairList,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+);
 
 ReactDOM.render(
-  <Provider store = {store}>
+  <Provider store={store}>
     <App />
-    </Provider>, 
-  document.getElementById("root"));
+  </Provider>,
+  document.getElementById("root")
+);
